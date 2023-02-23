25c468c23f2080e9ab21b836d269abcfd476093a
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-02-23 22:56:34 +0200

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int p;
+for (p = 0 ; p < 10 ; p++)
+{
+_putchar(p);
+}
+_putchar('\n');
+}
