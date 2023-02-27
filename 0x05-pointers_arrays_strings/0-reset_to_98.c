ac9bc5520a398b36eabbcfe441dede42ce2bea06
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-02-27 22:56:22 +0200

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- *
- *  reset_to_98 - a function that takes n and resets it to 98
- *  @n: input
- *  Return: n
- *
+ * reset_to_98 - a function that takes n and resets it to 98
+ * @n: input
+ * Return: n
  */
 void reset_to_98(int *n)
 {
-*n = 98
+*n = 98;
 }
