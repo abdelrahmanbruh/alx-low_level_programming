592c6648ad60f6009f438d3f45d96722f46a9e33
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-03-22 23:41:37 +0200

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,12 @@
 * @name: string to add
 * @f: pointer to function
 * Return: nothing
-* ***/
-void print_name(char *name, void (*f)(char *))
-{
-if (name == NULL || f == NULL)
-{
-return;
-}
-f(name);
-}
+**/
+	void print_name(char *name, void (*f)(char *))
+	{
+		if (name == NULL || f == NULL)
+			return;
+
+
+		f(name);
+	}
