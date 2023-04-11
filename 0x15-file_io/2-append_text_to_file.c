f34fb0dc8cb180b299660ed95168aed444f8c96c
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-04-11 09:13:30 +0200

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to the file
+ * @filename: file passed as an argument
+ * @text_content: string passed as an argument
+ * Return: 1 or -1 if fails
+ *
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int OP, WR, len = 0;
+OP = open(filename, O_WRONLY | O_APPEND);
+WR = write(OP, text_content, len);
+if (filename == NULL)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+{
+len++;
+}
+}
+if (OP == -1 || WR == -1)
+{
+return (-1);
+}
+close(OP);
+return (1);
+}
